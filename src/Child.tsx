2a80228526e03a105163f749090d4199608517c5
Olaoluwa Mustapha <ola.mustapha@serokell.io>
2023-09-19 15:58:35 -0500

    chore: Did a bit of cleanup

--- src/Child.tsx
+++ src/Child.tsx
@@ -1,15 +1,60 @@
-import styles from "./Child.module.scss"
+import styles from "./Child.module.scss";
+
+type C = Record<keyof typeof styles, string[]>;
+
+interface ChildProps {
+  customClasses: Partial<C>;
+}
+
+// This could become a helper
+const combineClasses = (...classes: string[]) => classes.join(" ");
+
+// This can be made generic
+const includeDefaults = (customClasses: Partial<C>) => {
+  const defaultCustomClasses = {
+    child: [],
+    childDiv1: [],
+    childDiv2: [],
+    childDiv1Mod9: [],
+  } satisfies C;
+
+  return {
+    ...defaultCustomClasses,
+    ...customClasses,
+  };
+};
 
 // Example of allowing to customise any style class
-export default ({mixClassName}: { mixClassName?: Partial<typeof styles>}) => 
+export default ({ customClasses }: ChildProps) => {
+  const customClassesWithDefaults = includeDefaults(customClasses);
+
   // TODO: this seems to be a repeated pattern, maybe we could have an auxiliary function
-  // defined in a type-safe way
-  // extended("someClassKey", styles, mixClassname)
-  <div className={`${styles.child} ${mixClassName?.child || ""}`}>
-    <div className={`${styles.childDiv1} ${mixClassName?.childDiv1 || ""}`}>
-      Div 1
+  return (
+    // defined in a type-safe way
+    // extended("someClassKey", styles, mixClassname)
+    <div
+      className={combineClasses(
+        styles.child,
+        ...customClassesWithDefaults.child
+      )}
+    >
+      <div
+        className={combineClasses(
+          styles.childDiv1,
+          ...customClassesWithDefaults.childDiv1
+        )}
+      >
+        Div 1
+      </div>
+
+      <div
+        className={combineClasses(
+          styles.childDiv2,
+          ...customClassesWithDefaults.childDiv2
+        )}
+      >
+        Div 2
+      </div>
     </div>
-    <div className={`${styles.childDiv2} ${mixClassName?.childDiv2 || ""}`}>
-      Div 2
-    </div>
-  </div>
+  );
+};
